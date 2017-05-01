1da97d82e94598c7bf272b2f5d87e44b54da0668
MathewChoi <mathew.m.choi@gmail.com>
2017-04-30 17:04:25 -0700

    Add files via upload

---
+++ Python/API/create_table.py
@@ -0,0 +1,14 @@
+import sqlite3
+
+connection = sqlite3.connect('data.db')
+cursor = connection.cursor()
+
+create_table = "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username text, password text, jwt text)"
+cursor.execute(create_table)
+
+create_table = "CREATE TABLE IF NOT EXISTS messages (sender text, receiver text, ciphertext text, time_stamp datetime, read int)"
+cursor.execute(create_table)
+
+connection.commit()
+
+connection.close()
