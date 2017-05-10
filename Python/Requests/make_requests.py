e48655bf5813a76e9be93422a97be7dcb7347ce0
MathewChoi <mathew.m.choi@gmail.com>
2017-05-10 13:06:32 -0700

    Add files via upload

--- Python/Requests/make_requests.py
+++ Python/Requests/make_requests.py
@@ -8,13 +8,13 @@ import json
 
 #declare constants... DO NOT CHANGE THE CONTENTS OF THESE VARIABLES
 class constants:
-    url = 'http://127.0.0.1:5000'
+    #local_url = 'http://127.0.0.1:5000'
+    site_url = 'http://www.mathewchoi.me'
 
-def register_user(url, emaill, username, password):
+def register_user(url, email, username, password):
     #setup the request route
     route = '/register'
-    request_url = constants.url + route
-    print("request_url = {}".format(request_url))
+    request_url = constants.site_url + route
 
     data = {
         'email' : email,
@@ -23,16 +23,12 @@ def register_user(url, emaill, username, password):
     }
     response = requests.post(request_url, data=data)
 
-    if response.text:
-        return response.text
-    else:
-        print("Oops, something went wrong. Sorry about that.")
+    return response.text
 
 def auth_user(url, username, password):
     #setup the request route
     route = '/auth'
-    request_url = constants.url + route
-    print("request_url = {}".format(request_url))
+    request_url = constants.site_url + route
 
     data = {
         "username" : username,
@@ -41,34 +37,24 @@ def auth_user(url, username, password):
 
     response = requests.post(request_url, data=data)
 
-    if response.text:
-        print(response.text, response)
-        return response.text
-    else:
-        print("Oops, something went wrong. Sorry about that.")
+    return response.text
 
 def refresh_token(url, refresh_tok):
     #setup the request route
     route = '/auth'
-    request_url = constants.url + route
-    print("request_url = {}".format(request_url))
+    request_url = constants.site_url + route
 
     headers = {
         "Authorization" : "Bearer "+refresh_tok
     }
     response = requests.put(request_url, headers=headers)
 
-    if response.text:
-        print(response.text, response)
-        return response.text
-    else:
-        print("Oops, something went wrong. Sorry about that.")
+    return response.text
 
 def send_message(url, sender, receiver, ciphertext, access_tok):
     #setup the request route
     route = '/message'
-    request_url = constants.url + route
-    print("request_url = {}".format(request_url))
+    request_url = constants.site_url + route
 
     headers = {
         "Authorization" : "Bearer "+access_tok,
@@ -82,17 +68,12 @@ def send_message(url, sender, receiver, ciphertext, access_tok):
     print("data = {}".format(data))
     response = requests.post(request_url, headers=headers, data=json.dumps(data))
 
-    if response.text:
-        print(response.text, response)
-        return response.text
-    else:
-        print("Oops, something went wrong. Sorry about that.")
+    return response.text
 
 def get_messages(url, receiver, access_tok):
     #setup the request route
     route = '/message/'
-    request_url = constants.url + route + str(receiver)
-    print("request_url = {}".format(request_url))
+    request_url = constants.site_url + route + str(receiver)
 
     headers = {
         "Authorization" : "Bearer "+access_tok
@@ -100,71 +81,22 @@ def get_messages(url, receiver, access_tok):
 
     response = requests.get(request_url, headers=headers)
 
-    if response.text:
-        print(response.text, response)
-        return response.text
-    else:
-        print("Oops, something went wrong. Sorry about that.")
+    return response.text
 
 
 """
     The following functions exist for testing purposes, and should be removed from the application before launch
 """
-def print_all_users(url):
+def get_all_users(url):
     route = '/users'
-    request_url = constants.url + route
+    request_url = constants.site_url + route
 
     response = requests.get(request_url)
 
-    if response.text:
-        print(response.text, response)
-    else:
-        print("An error has occurred. Sorry bout that.")
+    return response.text
 
 """
-    Driver
+    Test Driver
 """
-email = "detectivetim@gmail.com"
-sender = username = "tim"
-password = "choi"
-receiver = "ryan"
-receiver_pass = "riehl"
-ciphertext = "hey chump"
-
-# testing print_all_users
-print('printing all users')
-print_all_users(constants.url)
-
-#testing register_user
-register_user(constants.url, email, username, password)
-
-#testing print_all_users
-print('printing all users')
-print_all_users(constants.url)
-
-
-"""
-#testing auth_user
-print("testing user authentication")
-tokens = json.loads(auth_user(constants.url, username, password))
-
-access_tok = tokens['access token']
-refresh_tok = tokens['refresh token']
-print('access token = {}\n'.format(access_tok))
-print('refresh token = {}'.format(refresh_tok))
-
-#testing refresh_token
-new_token  = json.loads(refresh_token(constants.url, refresh_tok)) #generates a new 'fresh' access_token
-access_tok = new_token["access token"]
-print('[NEW] access token = {}\n'.format(access_tok))
-
-#testing post message
-#print("Trying to post a message")
-#post_msg = send_message(constants.url, sender, receiver, ciphertext, access_tok)
-
-print("getting receiver tokens")
-receiver_tokens = json.loads(auth_user(constants.url, receiver, receiver_pass))
-
-#testing get message
-get_msg = get_messages(constants.url, receiver, receiver_tokens['access token'])
-"""
+response_text = register_user(constants.site_url, "mathew.m.choi@gmail.com", "Mathew", "Choi")
+print("response_text = {}".format(response_text))
