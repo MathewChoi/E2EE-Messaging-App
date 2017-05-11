80683c460526aa3a41e1b21552db1cd956db24cf
Ryan Riehl <ryanpriehl@gmail.com>
2017-05-11 05:06:01 -0700

    Add files via upload
    Worked on all the menu options.

--- Python/Requests/chatroommenu.py
+++ Python/Requests/chatroommenu.py
@@ -1,11 +1,13 @@
-#from make_requests import auth_user, register_user, refresh_token, send_message, get_messages, get_all_users, constants
-from pathlib import Path
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives.asymmetric import rsa
+from make_requests import auth_user, register_user, refresh_token, send_message, get_messages, constants
 import json
-import RSAEncryptDecrypt
+import rsa_encrypt_decrypt
+import encrypt_decrypt
+import contacts
+import make_requests
+from message import Message
+from user import User, UserRegister
 
-PATH_TO_PASS_DICT = "C:\\Users\Ryan\PycharmProjects\EncryptedChat\password_dict.txt";
+PATH_TO_PASS_DICT = "C:\\Users\Ryan\PycharmProjects\EncryptedChat\password_dict.txt"
 
 
 class Registered_User:
@@ -34,11 +36,11 @@ def login_register():
         print("3. Exit")
 
         choice = input()
-        if (choice == "1"):
+        if choice == "1":
             login()
-        elif (choice == "2"):
+        elif choice == "2":
             register()
-        elif (choice == "3"):
+        elif choice == "3":
             print("Exiting program. Goodbye.")
             cont = False
         else:
@@ -52,22 +54,24 @@ def login():
     print("Password: ")
     password = input()
     response = auth_user(constants.url, username, password)
-    print("\nResponse:\n {}".format(response))
-    if ("access" in response):
-        #get access and refresh tokens from server
+    # print("\nResponse:\n {}".format(response))
+    if "access" in response:
+        # get access and refresh tokens from server
         tokens = json.loads(response)
         session_user.username = username
         session_user.access_tok = tokens["access token"]
         session_user.refresh_tok = tokens["refresh token"]
         session_user.print()
 
-        #read user info from files
-        session_user.contacted_users  = get_contacted_users(username)
-        session_user.private_keys  = get_private_keys(username)
-        session_user.public_keys  = get_public_keys(username)
+        # read user info from database
+        session_user.contacted_users = contacts.get_contacted_users(username)
+        session_user.private_keys = contacts.get_private_keys(username)
+        session_user.public_keys = contacts.get_public_keys(username)
 
-        #show second menu
+        # show second menu
         chatroom_menu()
+    else:
+        print("Login failed.")
 
 
 def register():
@@ -76,31 +80,15 @@ def register():
     username = input()
     print("Email: ")
     email = input()
-    print("Password: ")
     password = get_valid_password()
-
-    #storage file variables
-    filepath = "//home//mathew//Desktop//CECS478//phase4//clientside//files//"
-    contacted_filename = filepath+username+"_contacts.txt"
-    public_keys_filename = filepath+username+"_publickeys.txt"
-    private_keys_filename = filepath+username+"_privatekeys.txt"
-
-    #create new files
-    contacted_file = open(contacted_filename, "w")
-    public_keys_file = open(public_keys_filename, "w")
-    private_keys_file = open(private_keys_filename, "w")
-
-    #close files
-    contacted_file.close()
-    public_keys_file.close()
-    private_keys_file.close()
+    # TODO add the user to the DB
 
 
 def get_valid_password():
     print("Password: ")
     password = input()
     while password in open(PATH_TO_PASS_DICT).read() or len(password) < 8:
-        print("That password is too weak. Try again.\n")
+        print("That password is too short or too common. Try again.\n")
         print("Password: ")
         password = input()
     print("Confirm password: ")
@@ -125,126 +113,91 @@ def chatroom_menu():
         print("\nSelect an option:")
         print("1. Send a message")
         print("2. Check messages")
-        print("3. Add a contact")
-        print("4. Quit")
+        print("3. List contacts")
+        print("4. Add a contact")
+        print("5. Save a user's public key")
+        print("6. Quit")
 
         choice = input()
         if choice == "1":
-            send_msg()
+            send_message()
         elif choice == "2":
-            get_msg()
+            get_messages()
         elif choice == "3":
-            add_contact()
+            list_contacts()
         elif choice == "4":
+            add_contact()
+        elif choice == "5":
+            save_key()
+        elif choice == "6":
             print("Exiting program. Goodbye.")
             cont = False
         else:
-            print("Invalid input. Please enter 1, 2, or 3.")
+            print("Invalid input. Please enter an integer 1-6.")
 
 
-def get_contacted_users(username):
-    #storage file variables
-    filepath = "//home//mathew//Desktop//CECS478//phase4//clientside//files//"
-    filename = filepath+username+"_contacts.txt"
-    filepath = Path(filename)
-
-    #open and read files
-    if filepath.is_file():
-        contacted_file = open(filename)
-        for line in contacted_file:
-            session_user.contacted_users.append(line)
-        contacted_file.close()
+def send_message():
+    print("\nEnter the username of the person you want to message: ")
+    username = input()
+    if username in session_user.contacted_users:
+        print("Enter your message: ")
+        message = input()
+        json_object = encrypt_decrypt.encrypt(message, session_user.public_keys[username])
+        # TODO Not sure if I'm using the correct token here or the correct send message method
+        make_requests.send_message(constants.url, session_user.username, username, json_object, session_user.access_tok)
+        print("Message sent.")
     else:
-        contacted_file = open(filename, "w")
-        contacted_file.close()
+        print("That username is not in your contacts. They must send you their public key before you can message them.")
 
 
-def get_public_keys(username):
-    #storage file variables
-    filepath = "//home//mathew//Desktop//CECS478//phase4//clientside//files//"
-    filename = filepath+username+"_publickeys.txt"
-    filepath = Path(filename)
-
-    #open and read files
-    if filepath.is_file():
-        public_keys_file = open(filename)
-        content = ""
-        for line in public_keys_file:
-            content = content + line
-            session_user.public_keys = json.loads(content)
-        public_keys_file.close()
-    else:
-        public_keys_file = open(filename, "w")
-        public_keys_file.close()
+def check_messages():
+    # TODO Not sure if this will work
+    print("\nYour new messages:")
+    messages = Message.get(session_user.username)
+    for num in messages:
+        print(num, ": ")
+        for msg in messages[num]:
+            print(msg)
 
 
-def get_private_keys(username):
-    #storage file variables
-    filepath = "//home//mathew//Desktop//CECS478//phase4//clientside//files//"
-    filename = filepath+username+"_privatekeys.txt"
-    filepath = Path(filename)
-
-    #open files
-    if filepath.is_file():
-        private_keys_file = open(filename)
-        content = ""
-        for line in private_keys_file:
-            content = content + line
-            session_user.private_keys = json.loads(content)
-        private_keys_file.close()
-    else:
-        private_keys_file = open(filename, "w")
-        private_keys_file.close()
-
-
-def get_username_list():
-    usernames = json.loads(get_all_users(constants.url))
-    username_list = usernames['all usernames']
-    return username_list
-
-
-def send_msg():
-    print("Send a message: ")
-    username_list = get_username_list()
-    print("\nPlease select the username of the user you would like to communicate with from the following list.")
-    print(username_list)
-    choice = input()
-    if choice in username_list:
-        print("{} is in {}".format(choice, username_list))
-        if choice in session_user.contacted_users:
-            #get private key
-            if (len(session_user.private_keys)) is not 0:
-                private_key_path = session_user.private_keys[username]
-            another = True
-            if(another):
-                pass
-                #prompt user for message
-                #check message length
-                #encrypt message
-                #post message
-                #ask if they want to send another
-                    #no ==> another = False
-    else:
-        print("That is not a valid registered username. Try again..")
-
-
-def get_msg():
-    print("You entered '2': Get messages\n")
-    # get messages and display them
+def list_contacts():
+    print("\nYour current contacts are: ")
+    for c in session_user.contacted_users:
+        print(c)
 
 
 def add_contact():
     print("\nEnter the username of the contact to add: ")
     username = input()
-    # TODO need to check if contact is in the database
-    if username not in database:
+
+    user = User.find_by_username(username)
+    if user is None:
         print("That username doesn't exist.")
         return
     else:
-        # TODO need to get the email for the entered username
-        # email = username.get_email()
-        private_key = RSAEncryptDecrypt.generate_private_key()
-        # TODO Need to store this key in the DB
-        public_key = RSAEncryptDecrypt.get_public_key(private_key)
-        RSAEncryptDecrypt.send_public_key(private_key, email)
+        # Getting the public/private key pair
+        private_key = rsa_encrypt_decrypt.generate_private_key()
 
+        # Storing your private key in your contacts DB so you can decrypt their messages
+        contacts.add_contact(session_user.username, username, None, private_key)
+        # Sending your public key to the contact so they can message you
+        # TODO I'm not sure if this get email method will work
+        rsa_encrypt_decrypt.send_public_key(private_key, User.find_email_by_username(username))
+        print("Key sent. If they add you they'll be able to message you.")
+
+
+def save_key():
+    print("\nEnter the username of the contact: ")
+    username = input()
+    user = User.find_by_username(username)
+    if user is None:
+        print("That username doesn't exist.")
+        return
+    else:
+        public_key = rsa_encrypt_decrypt.get_key_input()
+        contacts.add_contact(session_user, username, public_key, None)
+
+
+session_user = Registered_User(username=0, access_tok=0, refresh_tok=0)
+
+login_register()
